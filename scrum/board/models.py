80748de810a3ba00c799f67f6311831da1dbae9f
Obsidian <ObsidianRock@users.noreply.github.com>
2017-04-06 13:19:40 +0100

    added model to scrum board

--- scrum/board/models.py
+++ scrum/board/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.utils.translation import ugettext_lazy as _
+
+
+class Sprint(models.Model):
+
+    name = models.CharField(max_length=100, blank=True, default='')
+    description = models.TextField(blank=True, default='')
+    end = models.DateField(unique=True)
+
+    def __str__(self):
+        return self.name or _('Sprint ending %s') % self.end
