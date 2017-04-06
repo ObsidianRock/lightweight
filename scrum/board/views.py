d979f1cea12ecc4eee5848d2695e8709d2ff278d
Obsidian <ObsidianRock@users.noreply.github.com>
2017-04-06 14:26:38 +0100

    added sprint api endpoint

--- scrum/board/views.py
+++ scrum/board/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets, authentication, permissions
+from .models import Sprint
+from .serializers import SprintSerializer
+
+
+class DefaultMixin:
+
+    authentication_class = (
+        authentication.BaseAuthentication,
+        authentication.TokenAuthentication,
+    )
+    permissions_classes = (
+        permissions.IsAuthenticated
+    )
+
+    paginate_by = 25
+    paginate_by_param = 'page_size'
+    max_paginate_by = 100
+
+
+class SprintViewSet(DefaultMixin, viewsets.ModelViewSet):
+
+    queryset = Sprint.Object.order_by('end')
+    serializer_class = SprintSerializer
+
+
